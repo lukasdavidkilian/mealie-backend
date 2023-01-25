bbdffd8c6655cfa6671eca9c8e0665d01ae45338
lukaskilian <lukasdavidkilian@gmail.com>
2023-01-25 22:14:34 +0100

    basic backend

--- mealie/mealie/views.py
+++ mealie/mealie/views.py
@@ -28,8 +28,8 @@ def mealie_list(request):
     goals = [protein_goal, fat_goal, carb_goal]
         # Use the provided goals and nutrition set to generate the meal plan
     meal_plan = generate_meal_plans(nutrition_set, goals)
-
-    return JsonResponse(meal_plan.__str__(),safe=False, status=status.HTTP_200_OK)
+    meal_plan.dict = json.loads(meal_plan)
+    return JsonResponse(meal_plan.dict,safe=False, status=status.HTTP_200_OK)
 
 
   #if request.method == 'POST':
